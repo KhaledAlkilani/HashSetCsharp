77651e13798a5780fe36c928a1e5195bebd2099e
KhaledAlkilani <khaledkelany@gmail.com>
2024-10-12 12:43:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace HashSetPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<int> warehouseA = new HashSet<int>();
+            HashSet<int> warehouseB = new HashSet<int>();
+
+            Console.WriteLine("Enter product IDs for warehouse A. 'Type done to finish'");
+            while (true)
+            {
+                string inputA = Console.ReadLine() ?? throw new Exception();
+
+                if (inputA.ToLower() == "done") break;
+
+                    string[] idsA = inputA.Split(',');
+                    foreach (var id in idsA)
+
+                    {
+                        if (int.TryParse(id.Trim(), out int inputWarehouseA))
+                        {
+                            warehouseA.Add(inputWarehouseA);
+                        }
+                    }
+            }
+
+            Console.WriteLine("Enter product IDs for warehouse B. ('Type done to finish')");
+            while (true)
+            {
+                string inputB = Console.ReadLine() ?? throw new Exception();
+
+                if (inputB.ToLower() == "done") break;
+
+                    string[] idsB = inputB.Split(",");
+                    foreach (var id in idsB)
+                    {
+                        if (int.TryParse(id.Trim(), out int inputWareHouseB))
+                        {
+                            warehouseB.Add(inputWareHouseB);
+                        }
+                    }   
+            }
+
+            HashSet<int> combinedHashSets = new HashSet<int>(warehouseA);
+            combinedHashSets.UnionWith(warehouseB);
+            
+            Console.WriteLine("Combined IDs:");
+            Console.Write(string.Join(", ", combinedHashSets));
+           
+        }
+    }
+}
